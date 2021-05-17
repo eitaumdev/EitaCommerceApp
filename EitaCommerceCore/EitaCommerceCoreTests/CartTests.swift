d043b21e0f7166932a15413a2c5b4278b1a0b42a
Marcos Vinicius Brito <marcosvcbrito@gmail.com>
2021-05-17 10:43:58 -0300

    Segundo video, explicação sobre testes

---
+++ EitaCommerceCore/EitaCommerceCoreTests/CartTests.swift
@@ -0,0 +1,35 @@
+//
+//  CartTests.swift
+//  EitaCommerceCoreTests
+//
+//  Created by marcos.brito on 16/05/21.
+//
+
+import XCTest
+
+class CartTests: XCTestCase {
+
+    override class func setUp() {
+        print("Eita um setup da classe")
+    }
+
+    override class func tearDown() {
+        print("Eita um teardown da classe")
+    }
+
+    override func setUpWithError() throws {
+        print("Eita um setup da instancia")
+    }
+
+    override func tearDownWithError() throws {
+        print("Eita um teardown da instancia")
+    }
+
+    func test1() {
+        print("Meu teste 1")
+    }
+
+    func test2() {
+        print("Meu teste 2")
+    }
+}
