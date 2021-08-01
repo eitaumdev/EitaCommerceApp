05ae7c1d8bf36e0cbe474e1f8718b3416a656d0e
Marcos Vinicius Brito <marcosvcbrito@gmail.com>
2021-08-01 13:34:23 -0300

    Separando o EitaCommerceCore em um arquivo.

--- EitaCommerceCore/EitaCommerceCoreTests/EitaCommerceCoreTests.swift
+++ EitaCommerceCore/EitaCommerceCoreTests/EitaCommerceCoreTests.swift
@@ -8,47 +8,6 @@
 import XCTest
 @testable import EitaCommerceCore
 
-public final class EitaCommerceCore<Item: CartItemEquatable> {
-    var cart = Cart(items: [Item]())
-    let addItemCartUseCase = AddItemCartUseCase<Item>()
-    let removeItemCartUseCase = RemoveItemCartUseCase<Item>()
-    let clearCartUseCase = ClearCartUseCase<Item>()
-    let calculatePriceCartUseCase = CalculatePriceCartUseCase<Item>()
-
-    var currentCart: Cart<Item> {
-        cart
-    }
-
-    public static func startWith(items: [Item]) -> EitaCommerceCore {
-        let eita = EitaCommerceCore()
-
-        items.forEach { item in
-            _ = eita.add(item: item)
-        }
-
-        return eita
-    }
-
-    func add(item: Item) -> Cart<Item> {
-        cart = addItemCartUseCase.execute(item, toCart: cart)
-        return cart
-    }
-
-    func remove(item: Item) -> Cart<Item> {
-        cart = removeItemCartUseCase.execute(item, toCart: cart)
-        return cart
-    }
-
-    func clear() -> Cart<Item> {
-        cart = clearCartUseCase.execute()
-        return cart
-    }
-
-    func calculateTotalPrice() -> Double {
-        return calculatePriceCartUseCase.execute(cart)
-    }
-}
-
 class EitaCommerceCoreTests: XCTestCase {
 
     let item = CartItem(item: Item(name: "Item 1", price: 10))
