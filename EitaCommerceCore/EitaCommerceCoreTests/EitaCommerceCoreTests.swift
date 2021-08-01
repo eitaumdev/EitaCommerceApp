4d67556be2d9bb5b9b68b074cbce964985d5ae9e
Marcos Vinicius Brito <marcosvcbrito@gmail.com>
2021-08-01 13:29:55 -0300

    Adicionando funcionalidade de limpar o carrinho

--- EitaCommerceCore/EitaCommerceCoreTests/EitaCommerceCoreTests.swift
+++ EitaCommerceCore/EitaCommerceCoreTests/EitaCommerceCoreTests.swift
@@ -12,6 +12,7 @@ public final class EitaCommerceCore<Item: CartItemEquatable> {
     var cart = Cart(items: [Item]())
     let addItemCartUseCase = AddItemCartUseCase<Item>()
     let removeItemCartUseCase = RemoveItemCartUseCase<Item>()
+    let clearCartUseCase = ClearCartUseCase<Item>()
 
     var currentCart: Cart<Item> {
         cart
@@ -34,6 +35,11 @@ public final class EitaCommerceCore<Item: CartItemEquatable> {
 
     func remove(item: Item) -> Cart<Item> {
         cart = removeItemCartUseCase.execute(item, toCart: cart)
+        return cart
+    }
+
+    func clear() -> Cart<Item> {
+        cart = clearCartUseCase.execute()
         return cart
     }
 }
@@ -73,12 +79,26 @@ class EitaCommerceCoreTests: XCTestCase {
         XCTAssertEqual(sut.currentCart.items, [item])
     }
 
-    func testEitaCommerceCore_removeItem_ShouldReturnCartWithoutItem() {
+    func testEitaCommerceCore_removeItem_ShouldReturnCartWithoutItems() {
         //Arrange
         let sut = EitaCommerceCore.startWith(items: [item])
 
         //Act
         let cart = sut.remove(item: item)
+
+        //Assert
+        XCTAssertEqual(cart.items.count, 0)
+        XCTAssertEqual(cart.items, [])
+        XCTAssertEqual(sut.currentCart.items.count, 0)
+        XCTAssertEqual(sut.currentCart.items, [])
+    }
+
+    func testEitaCommerceCore_clear_ShouldReturnCartWithoutItems() {
+        //Arrange
+        let sut = EitaCommerceCore.startWith(items: [item])
+
+        //Act
+        let cart = sut.clear()
 
         //Assert
         XCTAssertEqual(cart.items.count, 0)
