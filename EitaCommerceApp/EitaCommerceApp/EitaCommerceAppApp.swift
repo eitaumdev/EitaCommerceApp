eaca20b275fa449ce8a38080958a0e925569402b
Marcos Vinicius Brito <marcosvcbrito@gmail.com>
2021-08-11 15:34:01 -0300

    Criando o EitaCommerceApp

---
+++ EitaCommerceApp/EitaCommerceApp/EitaCommerceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EitaCommerceAppApp.swift
+//  EitaCommerceApp
+//
+//  Created by marcos.brito on 08/08/21.
+//
+
+import SwiftUI
+
+@main
+struct EitaCommerceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
